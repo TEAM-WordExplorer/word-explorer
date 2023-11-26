48035e424e4ad1a658d73cc7ba65345db6dac2d2
silver0108 <kk1170@naver.com>
2023-11-26 18:11:21 +0900

    feat: header 추가

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as BigLogoIcon } from "./icon/bigLogoIcon.svg";
+export { ReactComponent as MediumLogoIcon } from "./icon/mediumLogoIcon.svg";
+export { ReactComponent as SmallLogoIcon } from "./icon/smallLogoIcon.svg";
+export { ReactComponent as RightIcon } from "./icon/rightIcon.svg";
+export { ReactComponent as WrongIcon } from "./icon/wrongIcon.svg";
+export { ReactComponent as BackIcon } from "./icon/backIcon.svg";
