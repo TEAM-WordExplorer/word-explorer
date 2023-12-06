816d080545fe98834b422aa66dcdeadbc20981cc
silver0108 <kk1170@naver.com>
2023-12-07 03:34:07 +0900

    feat: logo icon 클릭 시 홈으로 이동

--- frontend/src/components/organism/Header/Header.tsx
+++ frontend/src/components/organism/Header/Header.tsx
@@ -1,10 +1,11 @@
 import { SmallLogoIcon } from "../../../assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <div style={{ display: "flex", justifyContent: "space-between", padding: "20px"}}>
-      <SmallLogoIcon/>
+      <SmallLogoIcon onClick={() => navigate('/')}/>
       <Link to='/' style={{ textDecoration: "none"}}>로그인</Link>
     </div>
   )
