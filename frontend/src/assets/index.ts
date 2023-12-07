9b5b3c52633913d7acc830a5abcd556a792578b4
silver0108 <kk1170@naver.com>
2023-12-07 15:29:46 +0900

    feat: 상황에 따른 header 변경

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -4,3 +4,4 @@ export { ReactComponent as SmallLogoIcon } from "./icon/smallLogoIcon.svg";
 export { ReactComponent as RightIcon } from "./icon/rightIcon.svg";
 export { ReactComponent as WrongIcon } from "./icon/wrongIcon.svg";
 export { ReactComponent as BackIcon } from "./icon/backIcon.svg";
+export { ReactComponent as MoveIcon } from "./icon/moveIcon.svg";
---
+++ frontend/src/components/organism/Header/stlyes.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react"
+
+export const HeaderWrapper = css`
+  display: flex;
+  justify-content: space-between;
+
+  height: 30px;
+  padding: 20px;
+`
