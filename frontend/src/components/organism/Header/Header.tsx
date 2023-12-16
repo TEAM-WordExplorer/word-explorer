1ea8b21b79f93606bdcafdaa1067bf931c4f9a53
silver0108 <kk1170@naver.com>
2023-12-16 18:14:04 +0900

    feat: 로그인 상태 관리

--- frontend/src/components/organism/Header/Header.tsx
+++ frontend/src/components/organism/Header/Header.tsx
@@ -3,20 +3,36 @@
 import { BackIcon, SmallLogoIcon } from "../../../assets";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { HeaderWrapper } from "./stlyes";
+import { useRecoilState} from "recoil";
+import { IsLoggedInState } from "../../../atom/LoginInfo";
 
 export default function Header() {
   const navigate = useNavigate();
   const location = useLocation();
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState(IsLoggedInState);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    navigate('/');
+  }
 
   return (
     <div css={HeaderWrapper}>
       {location.pathname === '/' ? (
         <>
           <SmallLogoIcon onClick={() => navigate('/')} />
-          <Link to='/login' style={{ textDecoration: "none"}}>로그인</Link>
+          {isLoggedIn ? (
+            <div onClick={handleLogout}>로그아웃</div>
+          ):(
+            <Link to = '/login' style = {{ textDecoration: "none" }}>로그인</Link>
+          )}
+          
         </>
       ):(
-        <BackIcon onClick={() => navigate(-1)}/>
+        <>
+          <BackIcon onClick={() => navigate(-1)}/>
+          <Link to='/' style={{ textDecoration: "none" }}>로그아웃</Link>
+        </>
       )}
       
     </div>
