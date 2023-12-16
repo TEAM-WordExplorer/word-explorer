ab96a01a0fa2e72b2f7743254571f995a145db22
soosbk <soobin851@kookmin.ac.kr>
2023-12-17 04:02:36 +0900

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home';
 import LikeWord from './pages/LikeWord/LikeWord';
-import WordDetail from './pages/LikeWord/page/WordDetail';
 import Quiz from './pages/Quiz/Quiz';
 import QuizDetail from './pages/Quiz/page/QuizDetail';
 import QuizResult from './pages/Quiz/page/QuizResult';
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/like' element={<LikeWord/>}></Route>
-        <Route path='/like/:word' element={<WordDetail/>}></Route>
         <Route path='/quiz' element={<Quiz/>}></Route>
         <Route path='/quiz/:id' element={<QuizDetail />}></Route>
         <Route path='/quiz/result' element={<QuizResult/>}></Route>
