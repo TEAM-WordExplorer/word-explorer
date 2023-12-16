11fe88c1fb40ecde3df8f5a26571bb125a4a1950
soosbk <soobin851@kookmin.ac.kr>
2023-12-16 16:06:35 +0900

    [modify] 파일수정

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,46 +1,63 @@
-/** @jsxImportSource @emotion/react */   // jsx pragma를 지정 -> css prop을 인식
+/** @jsxImportSource @emotion/react */
 import { useNavigate } from "react-router";
+import { useState } from "react";
 import ButtonGroup from "../../components/molecule/ButtonGroup/ButtonGroup";
 import InputForm from "../../components/molecule/InputForm/InputForm";
 import Header from "../../components/organism/Header/Header";
-import { theme } from "../../style/theme";
 import Introduce from "./component/Introduce";
 import WordResultBox from "./component/WordResultBox";
 import { homeStyle, contentStyle } from "./styles";
 
 export default function Home() {
-
   const navigate = useNavigate();
-
+  const [userInput, setUserInput] = useState<string>("");
+  const [wordList, setWordList] = useState<{ word: string; similarity: number }[]>([]);
+  
+  
   const ButtonClick1 = () => {
-    navigate('/like')
-  }
+    navigate('/like');
+  };
+
   const ButtonClick2 = () => {
-    navigate('/quiz')
-  }
+    navigate('/quiz');
+  };
 
-  // const wordList = [
-  //   {
-  //     word: "apple",
-  //     similarity: 10.04
-  //   },
-  //   {
-  //     word: "watermelon",
-  //     similarity: 12.12
-  //   },
-  //   {
-  //     word: "apple",
-  //     similarity: 10.04
-  //   },
-  // ]
+  const handleClick = async () => {
+    try {
 
-  const wordList: { word: string; similarity: number }[] = [];
-  // const newWordResult = { word: 'newWord', similarity: 0.9 };
-  // wordList.push(newWordResult);
+      console.log("userInput:", userInput);  // 추가된 부분
 
-  return(
+      const response = await fetch('http://localhost:8000/game/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ user_input: userInput }),
+      });
+      
+
+      if (response.ok) {
+        const result = await response.json();
+
+        // Assuming result is an array of results
+        if (Array.isArray(result.results)) {
+          // Assuming wordList state is an array of objects
+          setWordList(result.results);
+        } else {
+          console.error('Unexpected server response format');
+        }
+      } else {
+        console.error('Failed to process user input');
+      }
+    } catch (error) {
+      console.error('Error while making API call:', error);
+    }
+  };
+
+
+  return (
     <div css={homeStyle}>
-      <Header/>
+      <Header />
       <div css={contentStyle}>
         <ButtonGroup
           message1="좋아요 단어"
@@ -49,15 +66,15 @@ export default function Home() {
           onClick2={ButtonClick2}
         />
         <InputForm
-          message="추측하기" 
-          onClick={ButtonClick1}
+          message="추측하기"
+          onClick={handleClick}
           width="170px"
           borderRadius="15px"
         />
+        
         <WordResultBox wordResultList={wordList} />
-        <Introduce/>
+        <Introduce />
       </div>
-
     </div>
-  )
+  );
 }
