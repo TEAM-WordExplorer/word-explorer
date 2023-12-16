c30c17c166bd0416776714e0c4d3daff3ce2fb2b
silver0108 <kk1170@naver.com>
2023-12-16 18:22:53 +0900

    feat: 로그인 시 가능한 좋아요 기능 수정

--- frontend/src/pages/Home/component/AnswerModal.tsx
+++ frontend/src/pages/Home/component/AnswerModal.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { Modal } from "../../../components/molecule/Modal/Modal";
 import { AnswerModalContent1Style, AnswerModalContent2Style } from "./styles";
 import { getCsrfToken, postApi } from "../../../api/authService";
+import { useRecoilValue } from "recoil";
+import { IsLoggedInState } from "../../../atom/LoginInfo";
 
 interface LikeProps{
   word: string;
@@ -12,15 +14,19 @@ export default function AnswerModal(props: LikeProps) {
   const { word } = props;
 
   const [modalOpen, setModalOpen] = useState(true);
+  const isLoggedIn = useRecoilValue(IsLoggedInState);
 
   const handleModalClose = () => {
     setModalOpen(false);
   };
 
   const handleLikeSubmit = async () => {
-    const csrfToken = await getCsrfToken('url');
-    const response = postApi(csrfToken, 'url', word);
-    console.log(response);
+    if(isLoggedIn){
+      const csrfToken = await getCsrfToken('url');
+      const response = postApi(csrfToken, 'url', word);
+      console.log(response);
+    }
+    else alert("로그인이 필요합니다.")
   }
   return (
     <Modal isOpen={modalOpen} onClose={handleModalClose}>
