3fbe2077f7099e38147d62d23e2d567d0f81e01d
silver0108 <kk1170@naver.com>
2023-12-17 01:44:23 +0900

    feat: 단어 좋아요 기능

--- frontend/src/pages/Home/component/AnswerModal.tsx
+++ frontend/src/pages/Home/component/AnswerModal.tsx
@@ -1,17 +1,17 @@
 /** @jsxImportSource @emotion/react */
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Modal } from "../../../components/molecule/Modal/Modal";
 import { AnswerModalContent1Style, AnswerModalContent2Style } from "./styles";
-import { getCsrfToken, postApi } from "../../../api/authService";
 import { useRecoilValue } from "recoil";
 import { IsLoggedInState } from "../../../atom/LoginInfo";
+import axios from "axios";
 
 interface LikeProps{
-  word: string;
+  wid: number;
 }
 export default function AnswerModal(props: LikeProps) {
-  const { word } = props;
+  const { wid } = props;
 
   const [modalOpen, setModalOpen] = useState(true);
   const isLoggedIn = useRecoilValue(IsLoggedInState);
@@ -22,12 +22,17 @@ export default function AnswerModal(props: LikeProps) {
 
   const handleLikeSubmit = async () => {
     if(isLoggedIn){
-      const csrfToken = await getCsrfToken('url');
-      const response = postApi(csrfToken, 'url', word);
-      console.log(response);
+      try{
+        const response = axios.post('url', wid); // 이 부분 url 수정하면 됨
+        console.log(response);
+      }
+      catch(error){
+        console.log(error)
+      }
     }
     else alert("로그인이 필요합니다.")
   }
+
   return (
     <Modal isOpen={modalOpen} onClose={handleModalClose}>
       <div css={AnswerModalContent1Style}>정답!</div>
